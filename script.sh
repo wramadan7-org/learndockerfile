4d79d950044c8ce6ad2845a9edbb0fe8be63cf59
wramadan7-org <wramadan1203@gmail.com>
2022-12-18 22:46:18 +0700

    RUN Instruction

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 ## Create image
 ## -t is used for naming image
+
+## FROM Instruction
 docker build -t wramadan7/from from
+
+## RUN Instruction
+docker build -t wramadan7/run run
+
+## If we want to display detail, we can use command --progress=plain
+## If we want we want to repeat it again without cache we can use command --no-cache
+docker build -t wramadan7/run run --progress=plain --no-cache
