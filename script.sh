2e7b72911ebe54ca56a10c22169a03f13d422550
wramadan7-org <wramadan1203@gmail.com>
2022-12-18 23:02:27 +0700

    Command Instruction

--- script.sh
+++ script.sh
@@ -10,3 +10,18 @@ docker build -t wramadan7/run run
 ## If we want to display detail, we can use command --progress=plain
 ## If we want we want to repeat it again without cache we can use command --no-cache
 docker build -t wramadan7/run run --progress=plain --no-cache
+
+## CMD Instrution
+docker build -t wramadan7/command command
+
+## See detail image
+docker image inspect wramadan7/command
+
+## Create container
+docker container create --name command wramadan7/command
+
+## Start container
+docker container start command
+
+## Logs container
+docker container logs command
